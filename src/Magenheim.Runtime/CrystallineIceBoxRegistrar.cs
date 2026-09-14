f04ec8111210cdcf0579d22f019c31394f8f0d25
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-14 14:22:41 -0800

    Simplify Crystalline Ice Box recipe

--- src/Magenheim.Runtime/CrystallineIceBoxRegistrar.cs
+++ src/Magenheim.Runtime/CrystallineIceBoxRegistrar.cs
@@ -87,8 +87,6 @@ internal sealed class CrystallineIceBoxRegistrar : IDisposable
                 {
                     Cost("BlackMarble", 12),
                     Cost("Iron", 8),
-                    Cost("Silver", 8),
-                    Cost("Crystal", 12),
                     Cost("RefinedEitr", 5),
                     Cost("Magenheim_Crystal_Frost_Master", 1),
                 }
