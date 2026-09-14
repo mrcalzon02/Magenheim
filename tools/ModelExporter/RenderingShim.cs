049c5ac5eef6c41692af0870ed7a1a475150bceb
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-14 15:33:43 -0800

    Add Unity rendering shims for model export

---
+++ tools/ModelExporter/RenderingShim.cs
@@ -0,0 +1,19 @@
+namespace UnityEngine.Rendering;
+
+public enum BlendMode
+{
+    Zero = 0,
+    One = 1,
+    SrcColor = 3,
+    OneMinusSrcColor = 4,
+    SrcAlpha = 5,
+    OneMinusSrcAlpha = 10
+}
+
+public enum ShadowCastingMode
+{
+    Off = 0,
+    On = 1,
+    TwoSided = 2,
+    ShadowsOnly = 3
+}
