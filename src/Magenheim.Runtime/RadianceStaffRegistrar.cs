143ced122374d3d448bbc2407dfe935b01ae61d0
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-15 15:25:35 -0800

    Texture Radiance staff surfaces

--- src/Magenheim.Runtime/RadianceStaffRegistrar.cs
+++ src/Magenheim.Runtime/RadianceStaffRegistrar.cs
@@ -221,8 +221,8 @@ internal static class RadianceVisuals
         var ivory = Mat(source,"ivory",new Color(.78f,.72f,.58f,1f),.03f,.23f);
         var bronze = Mat(source,"bronze",new Color(.62f,.43f,.19f,1f),.46f,.28f);
         var gold = Mat(source,"gold",new Color(.90f,.65f,.18f,1f),.68f,.43f);
-        var light = Mat(source,"light",new Color(1f,.91f,.48f,1f),.02f,.78f,.42f);
-        var white = Mat(source,"white",new Color(1f,.99f,.86f,1f),.01f,.88f,.60f);
+        var light = Mat(source,"radiance-light-crystal",new Color(1f,.91f,.48f,1f),.02f,.78f,.42f);
+        var white = Mat(source,"radiance-white-crystal",new Color(1f,.99f,.86f,1f),.01f,.88f,.60f);
 
         if (assetName == "staff-radiance-simple")
         {
@@ -273,7 +273,7 @@ internal static class RadianceVisuals
     }
     private static Material Mat(Material source,string name,Color color,float metallic,float gloss,float emission=0f)
     {
-        var m=new Material(source){name="magenheim.radiance."+name}; m.mainTexture=Texture2D.whiteTexture; m.mainTextureScale=Vector2.one; m.mainTextureOffset=Vector2.zero; if(m.HasProperty("_Color"))m.SetColor("_Color",color); if(m.HasProperty("_Metallic"))m.SetFloat("_Metallic",metallic); if(m.HasProperty("_Glossiness"))m.SetFloat("_Glossiness",gloss); if(m.HasProperty("_BumpMap"))m.SetTexture("_BumpMap",null); m.DisableKeyword("_NORMALMAP"); if(m.HasProperty("_EmissionColor")&&emission>0f){m.SetColor("_EmissionColor",color*emission);m.EnableKeyword("_EMISSION");} else m.DisableKeyword("_EMISSION"); m.SetOverrideTag("RenderType","Opaque"); if(m.HasProperty("_ZWrite"))m.SetFloat("_ZWrite",1f); m.renderQueue=2000; return m;
+        var m=new Material(source){name="magenheim.radiance."+name}; GeneratedSurfaceTextures.Apply(m,name); if(m.HasProperty("_Color"))m.SetColor("_Color",color); if(m.HasProperty("_Metallic"))m.SetFloat("_Metallic",metallic); if(m.HasProperty("_Glossiness"))m.SetFloat("_Glossiness",gloss); if(m.HasProperty("_BumpMap"))m.SetTexture("_BumpMap",null); m.DisableKeyword("_NORMALMAP"); if(m.HasProperty("_EmissionColor")&&emission>0f){m.SetColor("_EmissionColor",color*emission);m.EnableKeyword("_EMISSION");} else m.DisableKeyword("_EMISSION"); m.SetOverrideTag("RenderType","Opaque"); if(m.HasProperty("_ZWrite"))m.SetFloat("_ZWrite",1f); m.renderQueue=2000; return m;
     }
     private static Mesh MakeBox(){var m=new Mesh{name="magenheim.radiance.box"};m.vertices=new[]{new Vector3(-.5f,-.5f,-.5f),new Vector3(.5f,-.5f,-.5f),new Vector3(.5f,.5f,-.5f),new Vector3(-.5f,.5f,-.5f),new Vector3(-.5f,-.5f,.5f),new Vector3(.5f,-.5f,.5f),new Vector3(.5f,.5f,.5f),new Vector3(-.5f,.5f,.5f)};m.triangles=new[]{0,3,2,0,2,1,4,5,6,4,6,7,0,4,7,0,7,3,1,2,6,1,6,5,0,1,5,0,5,4,3,7,6,3,6,2};m.RecalculateNormals();m.RecalculateBounds();return m;}
     private static Mesh MakeCylinder(int sides){var v=new List<Vector3>();var t=new List<int>();for(var r=0;r<2;r++){var y=r==0?-.5f:.5f;for(var i=0;i<sides;i++){var a=2f*Mathf.PI*i/sides;v.Add(new Vector3(.5f*Mathf.Cos(a),y,.5f*Mathf.Sin(a)));}}var b=v.Count;v.Add(new Vector3(0,-.5f,0));var top=v.Count;v.Add(new Vector3(0,.5f,0));for(var i=0;i<sides;i++){var n=(i+1)%sides;t.AddRange(new[]{i,sides+i,n,n,sides+i,sides+n,b,i,n,top,sides+n,sides+i});}var m=new Mesh{name="magenheim.radiance.cylinder."+sides,vertices=v.ToArray(),triangles=t.ToArray()};m.RecalculateNormals();m.RecalculateBounds();return m;}
