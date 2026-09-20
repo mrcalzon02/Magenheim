28eb3fc00414059b45182e2497f15e3cee6b237e
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-20 10:31:31 -0800

    Remove superseded transition harness entries

--- tests/Magenheim.Core.Tests/DefinitionAuthorityTests.cs
+++ tests/Magenheim.Core.Tests/DefinitionAuthorityTests.cs
@@ -69,8 +69,6 @@ internal static class DefinitionAuthorityTests
         assertions += UnderworldThermalExposureTests.Run();
         assertions += UnderworldThermalStateTests.Run();
         assertions += UnderworldGeothermalHazardTests.Run();
-        assertions += UnderworldTransitionTests.Run();
-        assertions += UnderworldTransitionStateCodecTests.Run();
         assertions += UnderworldTerrainLifecycleTests.Run();
         assertions += UnderworldMapProjectionTests.Run();
         assertions += UnderworldExplorationStateTests.Run();
