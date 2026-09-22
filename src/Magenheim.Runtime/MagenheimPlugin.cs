0efcf8cbe357a5d79a3312bf6863437c79d08748
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-21 18:57:16 -0800

    Bump Magenheim to 0.0.92

--- src/Magenheim.Runtime/MagenheimPlugin.cs
+++ src/Magenheim.Runtime/MagenheimPlugin.cs
@@ -17,7 +17,7 @@ internal sealed class MagenheimPlugin : BaseUnityPlugin
 {
     internal const string PluginGuid = "mrcalzon02.magenheim";
     internal const string PluginName = "Magenheim";
-    internal const string PluginVersion = "0.0.91";
+    internal const string PluginVersion = "0.0.92";
 
     private UnderworldCreaturePrototypeRegistrar? _underworldCreaturePrototypeRegistrar;
     private UnderworldAtmosphereRuntime? _underworldAtmosphereRuntime;
