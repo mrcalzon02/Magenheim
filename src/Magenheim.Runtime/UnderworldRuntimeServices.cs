6b3383ce3c33f5014e34b80cdd6e75a659334c65
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-22 20:12:29 -0800

    Register Fracture anchor tower family

--- src/Magenheim.Runtime/UnderworldRuntimeServices.cs
+++ src/Magenheim.Runtime/UnderworldRuntimeServices.cs
@@ -25,6 +25,7 @@ internal sealed class UnderworldRuntimeServices
         BiomeStructureResidency.Register(new FractureFaultLineFamily());
         BiomeStructureResidency.Register(new FractureCliffMonasteryFamily());
         BiomeStructureResidency.Register(new FractureSuspendedRoadStationFamily());
+        BiomeStructureResidency.Register(new FractureAnchorTowerFamily());
         BiomeStructureResidency.Register(new BlackwaterSparsePocketFamily());
         BiomeStructureResidency.Register(new FungalSplitPillarLandmarkFamily());
         BiomeStructureResidency.Register(new SulfurThreeLavafallsLandmarkFamily());
